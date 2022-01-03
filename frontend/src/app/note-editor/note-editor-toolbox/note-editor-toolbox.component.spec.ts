85da548f59ee836d436373fa2913a6a87d927b5b
Ivar Mahhonin <ivar.mahhonin@piwoot.com>
2022-01-04 00:21:10 +0100

    added tests for note editor toolbox component

--- frontend/src/app/note-editor/note-editor-toolbox/note-editor-toolbox.component.spec.ts
+++ frontend/src/app/note-editor/note-editor-toolbox/note-editor-toolbox.component.spec.ts
@@ -1,16 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NoteEditorToolboxComponent } from './note-editor-toolbox.component';
+import {NoteEditorToolboxComponent} from './note-editor-toolbox.component';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
-describe('NoteEditorToolboxComponent', () => {
+fdescribe('NoteEditorToolboxComponent', () => {
   let component: NoteEditorToolboxComponent;
   let fixture: ComponentFixture<NoteEditorToolboxComponent>;
 
+  const getIcon = (className: string) => {
+    fixture.detectChanges();
+    return fixture.nativeElement.querySelector(className);
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NoteEditorToolboxComponent ]
+      declarations: [NoteEditorToolboxComponent],
+      imports: [FontAwesomeModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +28,62 @@ describe('NoteEditorToolboxComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should properly render save button', () => {
+    component.canSave = true;
+    let saveIcon = getIcon('.save');
+    expect(saveIcon).toBeTruthy();
+
+    component.canSave = false;
+    saveIcon = getIcon('.save');
+    expect(saveIcon).toBeNull();
+  });
+
+  it('should properly render edit button', () => {
+    component.canEdit = true;
+    let editIcon = getIcon('.edit');
+    expect(editIcon).toBeTruthy();
+
+    component.canEdit = false;
+    editIcon = getIcon('.edit');
+    expect(editIcon).toBeNull();
+  });
+
+  it('should properly render remove button', () => {
+    component.canRemove = true;
+    let removeIcon = getIcon('.remove');
+    expect(removeIcon).toBeTruthy();
+
+    component.canRemove = false;
+    removeIcon = getIcon('.remove');
+    expect(removeIcon).toBeNull();
+  });
+
+  it('should emit event when save icon clicked', () => {
+    component.canSave = true;
+    const saveIcon = getIcon('.save');
+
+    spyOn(component.saveClicked, 'emit');
+    saveIcon.click();
+    expect(component.saveClicked.emit).toHaveBeenCalled();
+  });
+
+  it('should emit event when edit icon clicked', () => {
+    component.canEdit = true;
+    const editIcon = getIcon('.edit');
+
+    spyOn(component.editClicked, 'emit');
+    editIcon.click();
+    expect(component.editClicked.emit).toHaveBeenCalled();
+  });
+
+  it('should emit event when remove icon clicked', () => {
+    component.canRemove = true;
+    const removeIcon = getIcon('.remove');
+
+    spyOn(component.removeClicked, 'emit');
+    removeIcon.click();
+    expect(component.removeClicked.emit).toHaveBeenCalled();
   });
 });
--- frontend/src/app/note-editor/note-editor-toolbox/note-editor-toolbox.component.ts
+++ frontend/src/app/note-editor/note-editor-toolbox/note-editor-toolbox.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {faSave, faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
